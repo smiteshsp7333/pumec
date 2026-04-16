1e397d876f6d1ad349fe720f7b6c1e39eac3e50b
SmiteshP7333 <157452848+SmiteshP7333@users.noreply.github.com>
2026-04-16 12:33:10 +0530

    fix(sitemap): remove erroneous string quotes from sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,1 +1,11 @@
-"import { MetadataRoute } from 'next'; export default function sitemap(): MetadataRoute.Sitemap { return [ { url: 'https://pumec.com', lastModified: new Date(), changeFrequency: 'yearly', priority: 1, } ]; }" 
+import { MetadataRoute } from 'next';  
+export default function sitemap(): MetadataRoute.Sitemap {  
+  return [  
+    {  
+      url: 'https://pumec.com',  
+      lastModified: new Date(),  
+      changeFrequency: 'yearly',  
+      priority: 1,  
+    }  
+  ];  
+} 
